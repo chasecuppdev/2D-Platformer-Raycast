a51b8f18095f612c920291e846a0882955dd867b
Chase <chasecupp51@gmail.com>
2019-08-26 21:32:56 -0400

    Some first attempts at major refactoring
    Turning the player class into a finite state machine with more manageable components

---
+++ Assets/Scripts/Character/CharacterController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    private IState currentState;
+
+    private void Start()
+    {
+        SetState(new IdleState(this));
+    }
+
+    private void Update()
+    {
+        currentState.Tick();
+    }
+
+    public void SetState(IState state)
+    {
+        if (currentState != null)
+        {
+            currentState.OnStateExit();
+        }
+
+        currentState = state;
+        Debug.Log(gameObject.name + "is in " + state.GetType().Name);
+
+        if (currentState != null)
+        {
+            currentState.OnStateEnter();
+        }
+    }
+}
---
+++ Assets/Scripts/Character/IdleState.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleState : IState
+{
+    public IdleState(CharacterController character)
+    {
+
+    }
+
+    public void Tick()
+    {
+        
+    }
+
+    public void OnStateEnter()
+    {
+        
+    }
+
+    public void OnStateExit()
+    {
+
+    }
+}
