aee141911d510c2a7ff9d179449aab2ee2f858fb
Chase <chasecupp51@gmail.com>
2019-12-15 14:50:19 -0500

    Added Energy Bar for player attack and implemented cooldown

---
+++ Assets/Scripts/UI/HUD/EnergyCooldownTracker.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnergyCooldownTracker : MonoBehaviour
+{
+    [SerializeField]
+    Image cooldownImage;
+    [SerializeField]
+    Cooldown energyCooldown;
+
+    public void FixedUpdate()
+    {
+        if (energyCooldown.timer <= 0)
+        {
+            cooldownImage.fillAmount = 1;
+        }
+        else
+        {
+            cooldownImage.fillAmount = 1 - ((float)energyCooldown.timer / (float)energyCooldown.cooldown);
+        }
+        
+    }
+}
